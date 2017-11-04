0a4dfb0150f9bc1b79aa953e31ac12f037db32e8
Clark <cnielsen@trilogyed.com>
2017-11-04 16:31:36 -0700

    server side socket setup

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express = require("express");
-var path = require("path");
 var mongojs = require("mongojs");
 
+// mongo db
 var db = require("./schema.js");
 
-// Initialize Express
+// initialize web socket & server
 var app = express();
 var PORT = 3000;
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+io.on("connection", function(socket) {
+	// client tried to join a room
+	socket.on("join", function(data) {
+		console.log(`client connected to "${data}"`);
+
+		if (data) {
+			socket.room = data;
+			socket.join(data);
+
+			// shake hands
+			socket.emit("message", `welcome to room "${data}"`);
+		}
+	});
+
+	// client sent a message to server
+	socket.on("message", function(data) {
+		console.log(data);
+
+		// pass change onto every other client in same room
+		io.to(socket.room).emit("message", data);
+	});
+});
 
 app.use(express.static("app/public"));
 
@@ -14,7 +39,7 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./app/public/index.html"));
 });
 
-// Listen on port 3000
-app.listen(PORT, function() {
+// start localhost
+server.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
