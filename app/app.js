3519237e6e82f6862be723e113f5b1a3479410af
elthsu <elthsu@ucla.edu>
2017-11-06 11:48:50 -0800

    project page with react map

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var Main = require("./components/Main")
+var Test = require("./components/Test")
 
 ReactDOM.render(<Main />, document.getElementById("app"));
